Correct calculation of rougeL for non-Latin scripts

code/finetuning/finetune_flan.py
@@ -654,8 +654,7 @@ def main():
         # Some simple post-processing
         decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-        result = metric.compute(predictions=decoded_preds, references=decoded_labels,
-                                use_stemmer=True)
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels, tokenizer=lambda x: x.split())
         result = {k: round(v * 100, 4) for k, v in result.items()}
         prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
         result["gen_len"] = np.mean(prediction_lens)